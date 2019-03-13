7d06d95dacdf031db48dd8c88a8193a3506091af
Chanti <tgtechjava@gmail.com>
2019-03-13 10:29:21 +0530

    chanti commit3 with all types of autowire properties

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,10 @@ public class Test
 {
 	public static void main(String[] args) 
 	{
-		Resource res=new ClassPathResource("myconfig.xml");
+		//Resource res=new ClassPathResource("myconfig.xml");
+		//Resource res=new ClassPathResource("myconfig1.xml");
+		//Resource res=new ClassPathResource("myconfig2.xml");
+		Resource res=new ClassPathResource("myconfig3.xml");
 		BeanFactory factory=new XmlBeanFactory(res);
 		
 		DemoBean ob1=(DemoBean)factory.getBean("id2");
